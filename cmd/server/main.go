64afe6f734f1f37366da7654d540f6b73dd5c4e2
sakekasu <flatsdy0819@gmail.com>
2024-08-04 17:13:41 +0900

    add driver

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"sandbox-gql/ent"
 	"sandbox-gql/graph"
 
+    _ "github.com/go-sql-driver/mysql"
+
+	"entgo.io/ent/dialect"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 )
@@ -13,6 +18,31 @@ import (
 const defaultPort = "8080"
 
 func main() {
+
+	entOptions := []ent.Option{}
+	entOptions = append(entOptions, ent.Debug())
+	client, err := ent.Open(dialect.MySQL, "usr:usrpwd@tcp(db:3306)/db", entOptions...)
+	if err != nil {
+		log.Fatalf("failed opening connection to mysql: %v", err)
+	}
+	defer client.Close()
+	// Run the automatic migration tool to create all schema resources.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	ctx := context.Background()
+	u, err := client.Todo.
+		Create().
+		SetName("John").
+		SetEmail("hogehoge@gmail.com").
+		SetDone(false).
+		Save(ctx)
+	if err != nil {
+		log.Fatalf("failed creating user: %v", err)
+	}
+	log.Println("user was created: ", u)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
--- go.mod
+++ go.mod
@@ -13,11 +13,13 @@ require (
 
 require (
 	ariga.io/atlas v0.25.1-0.20240717145915-af51d3945208 // indirect
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/agext/levenshtein v1.2.1 // indirect
 	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
 	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
 	github.com/go-openapi/inflect v0.19.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
